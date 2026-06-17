9e8fd852ec3e7b2330ff9d0dd30c55104cd70ac7
avonafebrary <avona.febrary@mhs.unsoed.ac.id>
2026-06-17 12:35:59 +0700

    Update 05_cluster_3d.R

--- src_R/05_cluster_3d.R
+++ src_R/05_cluster_3d.R
@@ -1,1 +1,42 @@
+#Program 5
 
+#Fungsi menghitung jarak Euclidean
+hitungJarak <- function(titik1, titik2) {
+  jarak <- sqrt(sum((titik1 - titik2)^2))
+  return(jarak)
+}
+
+#Pusat cluster
+A <- c(2, 1, 3)
+B <- c(1, -4, 6)
+C <- c(-2, 3, -2)
+
+#Input titik U
+U <- as.numeric(strsplit(
+  readline("Masukkan x1 x2 x3: "),
+  " "
+)[[1]])
+
+# Validasi input
+if (length(U) != 3 || any(is.na(U))) {
+
+  cat("Input tidak valid! Masukkan tepat 3 angka.")
+
+} else {
+
+  #Memanggil fungsi
+  jarakA <- hitungJarak(U, A)
+  jarakB <- hitungJarak(U, B)
+  jarakC <- hitungJarak(U, C)
+
+  #Menentukan cluster
+  if (jarakA <= jarakB && jarakA <= jarakC) {
+    cluster <- "A"
+  } else if (jarakB <= jarakA && jarakB <= jarakC) {
+    cluster <- "B"
+  } else {
+    cluster <- "C"
+  }
+
+  cat("Titik U berada pada Cluster", cluster)
+}
