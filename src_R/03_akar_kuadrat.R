ccf5758bcbabab2e575012356e4a17a04773feda
rahmattejabaskara <rahmat.baskara@mhs.unsoed.ac.id>
2026-06-17 14:21:03 +0700

    03_akar_kuadrat.R

--- src_R/03_akar_kuadrat.R
+++ src_R/03_akar_kuadrat.R
@@ -1,17 +1,28 @@
-#Soal program 3
-#Input
-a <- as.numeric(readline("Masukkan nilai a: "))  #Membaca input dari pengguna (readline) lalu mengkonversi ke tipe numerik dengan as.numeric()
-b <- as.numeric(readline("Masukkan nilai b: "))  #Membaca input dari pengguna (readline) lalu mengkonversi ke tipe numerik dengan as.numeric()
-c <- as.numeric(readline("Masukkan nilai c: "))  #Membaca input dari pengguna (readline) lalu mengkonversi ke tipe numerik dengan as.numeric()
-#Proses
-D <- b^2 - 4*a*c           #Menghitung determinan
-#Output
-if (D >= 0) {             #Percabangan: blok { } dijalankan jika D ≥ 0
-  x1 <- (-b + sqrt(D)) / (2*a)  #Menghitung x1; fungsi sqrt() sudah tersedia langsung di R tanpa import tambahan
-  x2 <- (-b - sqrt(D)) / (2*a)  #Menghitung x2; fungsi sqrt() sudah tersedia langsung di R tanpa import tambahan
-  cat("Akar-akar real:\n")      #Menampilkan "Akar-akar real :"
-  cat(sprintf("  x1 = %.3f\n", x1))  #Menampilkan output; sprintf("%.3f", x1) memformat angka menjadi 3 desimal; \n adalah newline
-  cat(sprintf("  x2 = %.3f\n", x2))  #Menampilkan output; sprintf("%.3f", x1) memformat angka menjadi 3 desimal; \n adalah newline
-} else {    #Jika kondisi if tidak terpenuhi (D < 0)
-  cat("Persamaan ini hanya memiliki akar-akar imajiner.\n")  #Menampilkan pesan bahwa akar bersifat imajiner
+# Program Mencari Akar Persamaan Kuadrat
+a <- as.numeric(readline("Masukkan nilai a : "))
+b <- as.numeric(readline("Masukkan nilai b : "))
+c <- as.numeric(readline("Masukkan nilai c : "))
+
+D <- b^2 - 4*a*c
+
+if (D > 0) {
+  
+  x1 <- (-b + sqrt(D)) / (2*a)
+  x2 <- (-b - sqrt(D)) / (2*a)
+  
+  cat("Akar real berbeda\n")
+  cat(sprintf("x1 = %.3f\n", x1))
+  cat(sprintf("x2 = %.3f\n", x2))
+  
+} else if (D == 0) {
+  
+  x <- -b / (2*a)
+  
+  cat("Akar real kembar\n")
+  cat(sprintf("x1 = x2 = %.3f\n", x))
+  
+} else {
+  
+  cat("Persamaan memiliki akar-akar imajiner\n")
+  
 }
