2194541723659aecc94a88cd0d585d2a227e88cb
hilyasaflina-art <hilya.saflina@mhs.unsoed.ac.id>
2026-06-17 12:33:54 +0700

    Update 02_string_kalimat.py

--- src_python/02_string_kalimat.py
+++ src_python/02_string_kalimat.py
@@ -1,1 +1,20 @@
+# Soal Program 2
+# Program 2 - Membuat objek string K1 sampai K4
 
+# String yang mengandung tanda petik tunggal
+K1 = "Saya tak 'kan menyerah."
+
+# String yang mengandung tanda petik ganda
+K2 = 'Ia berkata, "Aku menyayangimu."'
+
+# String yang mengandung petik tunggal dan ganda
+K3 = "\"Coba jelaskan pengertian 'cross-validation' dalam Machine Learning!\""
+
+# String biasa
+K4 = "Surat keputusan itu bernomor 62/UN.34/19/2023."
+
+# Menampilkan isi string
+print(K1)
+print(K2)
+print(K3)
+print(K4)
