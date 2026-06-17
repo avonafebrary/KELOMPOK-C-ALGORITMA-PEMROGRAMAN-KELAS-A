cb79001450b266fef920a5c8c6d7adcf35873729
rahmattejabaskara <rahmat.baskara@mhs.unsoed.ac.id>
2026-06-17 17:27:49 +0700

    03_akar_kuadrat.py

--- src_python/03_akar_kuadrat.py
+++ src_python/03_akar_kuadrat.py
@@ -8,16 +8,12 @@ D = b**2 - 4*a*c
 if D > 0:
     x1 = (-b + (D**0.5)) / (2*a)
     x2 = (-b - (D**0.5)) / (2*a)
-
     print("Akar real berbeda")
     print("x1 = %.3f" % x1)
     print("x2 = %.3f" % x2)
-
 elif D == 0:
     x = -b / (2*a)
-
     print("Akar real kembar")
     print("x1 = x2 = %.3f" % x)
-
 else:
     print("Persamaan memiliki akar-akar imajiner")
