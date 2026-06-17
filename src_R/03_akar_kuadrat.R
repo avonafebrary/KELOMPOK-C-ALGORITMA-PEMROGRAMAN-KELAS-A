c025160b28900c36693f45718f9d8d6ccff3bc9a
rahmattejabaskara <rahmat.baskara@mhs.unsoed.ac.id>
2026-06-17 17:24:09 +0700

    03_akar_kuadrat.R

--- src_R/03_akar_kuadrat.R
+++ src_R/03_akar_kuadrat.R
@@ -6,23 +6,15 @@ c <- as.numeric(readline("Masukkan nilai c : "))
 D <- b^2 - 4*a*c
 
 if (D > 0) {
-  
   x1 <- (-b + sqrt(D)) / (2*a)
   x2 <- (-b - sqrt(D)) / (2*a)
-  
   cat("Akar real berbeda\n")
   cat(sprintf("x1 = %.3f\n", x1))
   cat(sprintf("x2 = %.3f\n", x2))
-  
 } else if (D == 0) {
-  
   x <- -b / (2*a)
-  
   cat("Akar real kembar\n")
   cat(sprintf("x1 = x2 = %.3f\n", x))
-  
 } else {
-  
   cat("Persamaan memiliki akar-akar imajiner\n")
-  
 }
